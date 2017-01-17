a389ec6978f9ba12aff00283e5cb875eaeb16285
Fredrik Christenson <fredrik.christenson@ticnet.se>
2017-01-17 17:50:51 +0100

    WP

--- libs/declarations/__tests__/declarations_test.js
+++ libs/declarations/__tests__/declarations_test.js
@@ -1,12 +1,37 @@
 var D = require('../');
+var util = require('util');
 var path = require('path');
 var walk = require('../../directories').walk;
 var expect = require('chai').expect;
 
-describe('declarations', function () {
-  it('has a module', function () {
+describe('declarations', function() {
+  it('has a module', function() {
     expect(D).to.be.ok;
   });
 
+  describe('findDeclarations', function() {
+    it('walks the ast node tree and finds all declarations', function() {
+      var data = walk(path.join(__dirname, 'testing_dir'));
+      var stats = D.findDeclarations(data);
+      expect(stats.length).to.equal(3);
+      expect(stats).to.include('foo');
+      expect(stats).to.include('bar');
+      expect(stats).to.include('baz');
+    });
 
+    it('only picks up declaration', function() {
+      var data = walk(path.join(__dirname, 'testing_dir2'));
+      var stats = D.findDeclarations(data);
+      expect(stats.length).to.equal(3);
+      expect(stats).to.include('foo');
+      expect(stats).to.include('bar');
+      expect(stats).to.include('baz');
+    });
+  });
+
+  describe('findUnusedDeclaration', function() {
+    it('returns a list of unused declarations', function() {
+      expect(1).to.equal(2);
+    });
+  });
 });
--- libs/declarations/index.js
+++ libs/declarations/index.js
@@ -1,8 +1,96 @@
-// String->[AstData]->Number
-function countUsages (name, nodes) {
-  return findNodesWithContent(name, nodes).length;
+var get = require('lodash').get;
+var tail = require('lodash').tail;
+var head = require('lodash').head;
+
+// [AstData]->[String]
+function findDeclarations(nodes) {
+  return findDeclarationNodes(nodes)
+  .reduce(nodesToNames, [])
+  .filter(removeEmptyString);
+}
+
+// []->AstData->
+function nodesToNames(acc, node) {
+  acc.push(varDeclarationNodeToName(node));
+  acc.push(funcDeclarationNodeToName(node));
+  acc.push(mixinDeclarationNodeToName(node));
+
+  return acc;
+}
+
+function removeEmptyString(str) {
+  return get(str, 'length', 0) > 0;
+}
+
+function funcDeclarationNodeToName(node) {
+  var childNode = get(node, 'content[2]', {});
+
+  if (childNode.type === 'function') {
+    return get(childNode, 'content[0].content', '');
+  }
+
+  return '';
+}
+
+// AstData->String
+function mixinDeclarationNodeToName(node) {
+  var type = get(node, 'type');
+
+  if(type === 'mixin') {
+    return get(node, 'content[2].content');
+  }
+
+  return '';
+}
+
+// AstData->String
+function varDeclarationNodeToName(node) {
+  var childNode = get(node, 'content[0].content[0]', {});
+
+  if(childNode.type === 'variable') {
+    return get(childNode, 'content[0].content');
+  }
+
+  return '';
+}
+
+// [AstData]->[AstData]
+function findDeclarationNodes(nodes, acc) {
+  var result = Array.isArray(acc) ? acc : [];
+  
+  if(!Array.isArray(nodes)) return result;
+  if(nodes.length <= 0) return result;
+
+  var firstNode = head(nodes);
+  var content = get(firstNode, 'content', []);
+  if(isDeclarationNode(firstNode) || isMixinDeclarationNode(firstNode) || isFunctionDeclarationNode(firstNode)) {
+    result.push(firstNode);
+  }
+    
+  if(nodeHasChildren(firstNode)) result = result.concat(findDeclarationNodes(content));
+
+  return findDeclarationNodes(tail(nodes), result);
+}
+
+function isFunctionDeclarationNode(node) {
+  return get(node, 'type') === 'atrule'; 
+}
+
+function isMixinDeclarationNode(node) {
+  return get(node, 'type') === 'mixin'; 
+}
+
+// AstData->bool
+function nodeHasChildren(node) {
+  var content = get(node, 'content');
+  return Array.isArray(content) && content.length > 0;
+}
+
+// AstData->bool
+function isDeclarationNode(node) {
+  return get(node, 'type') === 'declaration';
 }
 
 module.exports = {
-  countUsages: countUsages
+  findDeclarations: findDeclarations
 };
